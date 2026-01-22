4ba415ce7c1413efe12996d31ad3e0ff98e7a743
Srinivas Gogula <75725628+srinivasgogula02@users.noreply.github.com>
2026-01-22 21:26:15 +0530

    Update page.tsx

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
             src={HeroBackground}
             alt="Background"
             fill
-            className="object-cover opacity-90"
+            className="object-cover opacity-30"
             priority
             placeholder="blur"
             sizes="100vw"
